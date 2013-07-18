45483b1eb3781643b94d5ac43a581430031a5863
IOS Student <student@techs-imac-13.fhda.edu>
2013-07-18 08:10:44 -0700

    Initial Commit

---
+++ CowGame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CowGame
+//
+//  Created by IOS Student on 7/18/13.
+//  Copyright (c) 2013 edu.foothill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CowGame/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CowGame
+//
+//  Created by IOS Student on 7/18/13.
+//  Copyright (c) 2013 edu.foothill. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
